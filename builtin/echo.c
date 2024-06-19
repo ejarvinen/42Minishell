489a70284d0acdde6e6d67aa61626fbb66303730
Sahra Taskinen <sahra.taskinen@gmail.com>
2024-06-19 13:50:56 +0300

    update echo

--- builtin/echo.c
+++ builtin/echo.c
@@ -6,13 +6,30 @@
 /*   By: sataskin <sataskin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 11:45:58 by sataskin          #+#    #+#             */
-/*   Updated: 2024/06/05 09:34:32 by sataskin         ###   ########.fr       */
+/*   Updated: 2024/06/17 14:46:22 by sataskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_echo(t_mini *shell, char **str)
+static int	check_newline(char *str)
 {
+	int	i;
+
+	i = 1;
+	while (str[i] != NULL)
+	{
 		
+	}	
+}
+
+void	ft_echo(t_mini *shell)
+{
+	int	nl;
+
+	nl = check_newline(shell->cmds->command);
+	if (shell->cmds->fd_outfile == 0)
+		to_terminal(shell, shell->cmds->command, 0, nl);
+	else
+		to_file(shell, shell->cmds->command, shell->cmds->fd_outfile, nl);
 }
