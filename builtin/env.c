3443c3c133b458add3fb1da9052c5ef70c557d0a
Sahra Taskinen <sataskin@c1r2p5.hive.fi>
2024-07-10 09:19:29 +0300

    env without comp fail

--- builtin/env.c
+++ builtin/env.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: sataskin <sataskin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 13:49:54 by sataskin          #+#    #+#             */
-/*   Updated: 2024/07/09 11:25:04 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/07/10 08:24:08 by sataskin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 
 int	ft_env(t_mini *shell, t_cmds *cmd)
 {
@@ -21,14 +22,14 @@ int	ft_env(t_mini *shell, t_cmds *cmd)
 	{
 		if (temp->equal == 1)
 		{
-			//ft_putstr_fd(temp->key, cmd->fd_outfile);
+			/*if (cmd->fd_outfile[0] > 1)
+				print_to_file(temp, cmd);
+			else
+				print_to_terminal(temp, cmd);*/
 			ft_putstr_fd(temp->key, cmd->fd_outfile[0]);
-			//ft_putstr_fd("=", cmd->fd_outfile);
 			ft_putstr_fd("=", cmd->fd_outfile[0]);
 			if (temp->value != NULL)
 				ft_putstr_fd(temp->value, cmd->fd_outfile[0]);
-				//ft_putstr_fd(temp->value, cmd->fd_outfile);
-			//ft_putchar_fd('\n', cmd->fd_outfile);
 			ft_putchar_fd('\n', cmd->fd_outfile[0]);
 		}
 		temp = temp->next;
@@ -40,3 +41,37 @@ int	ft_env(t_mini *shell, t_cmds *cmd)
 	}
 	return (0);
 }
+
+/*
+static void	print_to_file(t_env *temp, t_cmds *cmd)
+{
+		ft_putstr_fd(temp->key, cmd->fd_outfile[0]);
+		ft_putstr_fd("=", cmd->fd_outfile[0]);
+		if (temp->value != NULL)
+			ft_putstr_fd(temp->value, cmd->fd_outfile[0]);
+		ft_putchar_fd('\n', cmd->fd_outfile[0]);
+}
+
+int	ft_env(t_mini *shell, t_cmds *cmd)
+{
+	t_env	*temp;
+	
+	temp = shell->env;
+	while(temp)
+	{
+		if (temp->equal == 1)
+		{
+			if (cmd->fd_outfile[0] > 1)
+				print_to_file(temp, cmd);
+			else
+				print_to_terminal(temp);
+		}
+		temp = temp->next;
+	}
+	if (cmd->c_pid != -1)
+	{
+		free_data(shell, NULL);
+		exit(0);
+	}
+	return (0);
+}*/
