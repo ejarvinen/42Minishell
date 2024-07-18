2dc92f78e1b808d8e758cc24a50e66c7e76f56eb
Emmi Jarvinen <emansoor@c1r2p3.hive.fi>
2024-07-18 12:02:17 +0300

    can now run absolute paths again

--- execute/execute.c
+++ execute/execute.c
@@ -6,7 +6,7 @@
 /*   By: emansoor <emansoor@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 09:34:24 by emansoor          #+#    #+#             */
-/*   Updated: 2024/07/18 08:42:32 by emansoor         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:01:02 by emansoor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	safe_to_run(t_cmds *cmds)
 {
 	if (cmds->command == NULL
-		|| ((cmds->fd_infile == -1 || cmds->valid < 0)
+		|| ((cmds->fd_infile == -1 || cmds->valid == -1)
 			&& cmds->heredoc == NULL) || cmds->fd_outfile[0] < 0)
 		return (0);
 	return (1);
